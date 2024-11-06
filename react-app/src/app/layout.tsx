88c316ca1fd91d35e32e36d97e5485681981ee53
1dontkillme <abldtesttt@gmail.com>
2024-11-07 01:06:09 +0300

    added favicon

--- react-app/src/app/layout.tsx
+++ react-app/src/app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="/assets/icons/favicon.png" type="image/x-icon" />
+      </head>
       <body>
         <Header />
         {children}
