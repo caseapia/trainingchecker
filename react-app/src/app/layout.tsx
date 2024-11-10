f129f5e85fe9863e1135dfb88f0fcbfb2d6105d6
1dontkillme <abldtesttt@gmail.com>
2024-11-10 23:07:39 +0300

    added yandex metrika

--- react-app/src/app/layout.tsx
+++ react-app/src/app/layout.tsx
@@ -1,27 +1,51 @@
-"use client"
+"use client";
 import "./globals.css";
 import { Header } from "../components/Header/Header";
 import Notify from "../components/Notify/Notify";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
-  const [isOpened, setIsOpened] = useState<boolean>(false);
+}) {
 
-  const handleClose = () => {
-    setIsOpened(false);
-  }
-  const handleOpen = () => {
-    setIsOpened(true);
-  }
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.innerHTML = `
+      (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){
+          (m[i].a=m[i].a||[]).push(arguments)
+        };
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {
+          if (document.scripts[j].src === r) { return; }
+        }
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0];
+        k.async=1;
+        k.src=r;
+        a.parentNode.insertBefore(k,a);
+      })(window, document, "script", "https://mc.webvisor.org/metrika/tag_ww.js", "ym");
+      ym(98876766, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true,
+        trackHash:true
+      });
+    `;
+    document.head.appendChild(script);
+  }, []);
 
   return (
     <html lang="en">
       <head>
-        <link rel="shortcut icon" href="/assets/icons/favicon.png" type="image/x-icon" />
+        <link
+          rel="shortcut icon"
+          href="/assets/icons/favicon.png"
+          type="image/x-icon"
+        />
       </head>
       <body>
         <Header />
