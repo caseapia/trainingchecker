a2fa10f2fb4d4cc375616ace5cc5c027eec6d0e2
1dontkillme <abldtesttt@gmail.com>
2024-11-25 15:10:55 +0300

    worldlist test

--- src/app/worldlist/page.tsx
+++ src/app/worldlist/page.tsx
@@ -18,7 +18,7 @@ const WorldList = () => {
       }
 
       try {
-        const response = await fetch(url);
+        const response = await fetch(url, { mode: 'no-cors' });
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
