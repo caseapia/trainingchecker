1dcffdd4b208971954bb341443569024b477abbe
1dontkillme <abldtesttt@gmail.com>
2024-12-01 23:16:18 +0300

    1/1

--- src/app/changelog/page.tsx
+++ src/app/changelog/page.tsx
@@ -10,6 +10,7 @@ import { changeLog } from "@/consts/changelog";
 import Button from "@/components/Buttons/Button";
 import { FaClock, FaUser } from "react-icons/fa6";
 import Link from "next/link";
+import BootstrapTooltip from "@/components/Styles/TooltipStyles";
 
 const Changelog = () => {
   const [isLoaded, setIsLoaded] = useState<boolean>(true);
@@ -25,36 +26,44 @@ const Changelog = () => {
 
   return isLoaded ? (
     <PageWrapper title="Список изменений" classname={styles.NewWrapper}>
-      {changeLog.length > 0 ? (
-        changeLog.map((log, index) => (
-          <div
-            className={styles.log__item}
-            key={index}
-            style={{ background: `linear-gradient(160deg, ${log.color} 0%, #0000008c 100%)` }}
-          >
-            <div className={styles.log__item__icon_container}>
-              <p>{log.icon}</p>
-            </div>
-            <div className={styles.log__info_container}>
-              <div className={styles.log__info_container__text}>
-                <h3 className={styles.log__info_container__text__title}>{log.title}</h3>
-                <p className={styles.log__info_container__text__date}>
-                  <FaClock /> {log.date}&nbsp;<FaUser /> {log.author}
-                </p>
+      <div className={styles.blockWrapper}>
+        {changeLog.length > 0 ? (
+          changeLog.map((log, index) => (
+            <div
+              className={styles.log__item}
+              key={index}
+              style={{ background: `linear-gradient(160deg, ${log.color} 0%, #0000008c 100%)` }}
+            >
+              <div className={styles.log__item__icon_container}>
+                <p>{log.icon}</p>
               </div>
-              <div className={styles.log__info_container__buttonContainer}>
-                <Link href={`/changelog/${log.route}`}>
-                  <Button btnType="Transparent" text="Читать" type="button" style={{ borderColor: `${log.color}` }} />
-                </Link>
+              <div className={styles.log__info_container}>
+                <div className={styles.log__info_container__text}>
+                  <BootstrapTooltip 
+                    title={log.title}
+                    placement="top"
+                    enterDelay={0}
+                  >
+                    <h3 className={styles.log__info_container__text__title}>{log.title}</h3>
+                  </BootstrapTooltip>
+                  <span className={styles.log__info_container__text__data}>
+                    <FaClock /> {log.date}&nbsp;<FaUser /> {log.author}
+                  </span>
+                </div>
+                <div className={styles.log__info_container__buttonContainer}>
+                  <Link href={`/changelog/${log.route}`}>
+                    <Button btnType="Transparent" text="Читать" type="button" style={{ borderColor: `${log.color}` }} />
+                  </Link>
+                </div>
               </div>
             </div>
+          ))
+        ) : (
+          <div className={styles.log__empty}>
+            <p>Список изменений пуст</p>
           </div>
-        ))
-      ) : (
-        <div className={styles.log__empty}>
-          <p>Список изменений пуст</p>
-        </div>
-      )}
+        )}
+      </div>
     </PageWrapper>
   ) : (
     <div className={styles.PageWrapper}>
--- src/shared/consts/changelog.ts
+++ src/shared/consts/changelog.ts
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaHammer } from "react-icons/fa";
 import { FaGear } from "react-icons/fa6";
 
 type Props = {
@@ -12,12 +13,20 @@ type Props = {
 
 export const changeLog: Props[] = [
   {
-    route: "10-02-2023",
+    route: "12-01-2024",
     title: "Новое оформление страницы изменений",
     author: "dontkillme",
-    date: "10-02-2023",
+    date: "10-02-2024",
     color: "#f01f4b",
     icon: React.createElement(FaGear),
+  },
+  {
+    route: "12-01-2024",
+    title: "Обновления, опубликованные до новой системы патчноутов",
+    author: "dontkillme",
+    date: "12-01-2024",
+    color: "rgb(31 240 145)",
+    icon: React.createElement(FaHammer),
   },
 ];
 
