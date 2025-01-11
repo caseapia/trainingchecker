afa2f1f359174bb505b36faf5ee79a79209e8835
caseapia <abldtesttt@gmail.com>
2025-01-11 20:55:37 +0300

    upd: tooltip updates

--- src/components/Styles/TooltipStyles.tsx
+++ src/components/Styles/TooltipStyles.tsx
@@ -1,13 +1,33 @@
 import { Tooltip, tooltipClasses, styled, TooltipProps } from '@mui/material';
+import Fade from '@mui/material/Fade';
 
 export const BootstrapTooltip = styled(({ className, ...props }: TooltipProps) => (
-    <Tooltip {...props} arrow classes={{ popper: className }} />
+    <Tooltip
+      placement='top'
+      leaveDelay={100}
+      enterDelay={0}
+      {...props}
+      classes={{ popper: className }}
+      slots={{
+        transition: Fade,
+      }}
+      slotProps={{
+        transition: { timeout: 300, },
+      }}
+    />
 ))(({ theme }) => ({
     [`& .${tooltipClasses.arrow}`]: {
-      color: theme.palette.common.black,
+      color: 'rgba(32, 32, 32, 0.9)',
+      backdropFilter: 'blur(10px)',
     },
     [`& .${tooltipClasses.tooltip}`]: {
-      backgroundColor: theme.palette.common.black,
+      backgroundColor: 'rgba(32, 32, 32, 0.9)',
+      padding: '8px 12px',
+      borderRadius: '6px',
+      fontSize: '13px',
+      backdropFilter: 'blur(10px)',
+      boxShadow: '0 2px 10px rgba(0, 0, 0, 0.2)',
+      border: '1px solid black',
     },
 }));
 
