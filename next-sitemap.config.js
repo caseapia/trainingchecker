46e090326dd94be34cd52c0c09d693dc9503dd69
dontkillme <abldtesttt@gmail.com>
2025-05-08 11:33:54 +0300

    add: search engines indexing

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://trainingchecker.vercel.app',
+  generateRobotsTxt: true, // автоматически создаст robots.txt
+};
