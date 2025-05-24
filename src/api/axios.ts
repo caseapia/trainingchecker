12e95f94cae8c8c5e959d21ab6f65a39c76b17ea
dontkillme <abldtesttt@gmail.com>
2025-05-24 21:13:44 +0300

    add: hash sender as "authorization" header for chrono api

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,12 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 const TRAININGAPI_URL = process.env.NEXT_PUBLIC_TRAININGAPI_URL!;
 const CHRONOAPI_URL = process.env.NEXT_PUBLIC_CHRONOAPI_URL!;
 const METRICAPI_URL = "https://ptb.discord.com/api/webhooks/";
+const hash = Cookies.get("__ha_sh");
+
+console.log(hash);
 
 if (!TRAININGAPI_URL || !CHRONOAPI_URL) {
   throw new Error("API URLs are not properly defined.");
@@ -13,6 +17,14 @@ const createApiClient = (baseURL: string) => axios.create({
   headers: { "Content-Type": "application/json" },
 });
 
+const createAuthorizationApiClient = (baseURL: string) => axios.create({
+  baseURL,
+  headers: {
+    "Content-Type": "application/json",
+    "Authorization": hash,
+  },
+})
+
 export const trainingApiClient = createApiClient(TRAININGAPI_URL);
-export const chronoApiClient = createApiClient(CHRONOAPI_URL);
+export const chronoApiClient = createAuthorizationApiClient(CHRONOAPI_URL);
 export const metricApiClient = createApiClient(METRICAPI_URL);
