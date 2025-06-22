98e45721763cce008f38061a0aaa69a5e6f08c7e
caseapia <abldtesttt@gmail.com>
2025-06-23 02:00:05 +0300

    Possibly fixed building errors

--- src/i18n/i18nInstance.ts
+++ src/i18n/i18nInstance.ts
@@ -1,16 +1,14 @@
 import i18n from "i18next";
 import Backend from "i18next-http-backend";
-import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
-import { isDevModeEnabled } from "@/utils/metric";
 
 if (!i18n.isInitialized) {
   i18n
     .use(Backend)
-    .use(LanguageDetector)
     .use(initReactI18next)
     .init({
       fallbackLng: "en",
+      lng: typeof window !== "undefined" ? navigator.language.split("-")[0] : "en",
       supportedLngs: ["en", "ru"],
       ns: [
         "common",
@@ -29,14 +27,9 @@ if (!i18n.isInitialized) {
       backend: {
         loadPath: "/locales/{{lng}}/{{ns}}.json",
       },
-      detection: {
-        order: ["navigator", "htmlTag", "path", "subdomain"],
-        caches: [],
-      },
       react: {
-        useSuspense: true,
+        useSuspense: false,
       },
-      debug: isDevModeEnabled,
     });
 }
 
