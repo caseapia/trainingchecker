cd858cf2fc291b1fb3794351892e0aad0fde5fa5
caseapia <abldtesttt@gmail.com>
2025-06-24 02:30:09 +0300

    fix: Type error in formatUnixDate helper

--- src/utils/helpers/formatUnixDate.ts
+++ src/utils/helpers/formatUnixDate.ts
@@ -1,10 +1,10 @@
 import i18n from "i18next";
 
 export default function formatUnixDate(unixTimestamp: number): string {
-  const tSuffix = (key: string) => i18n.t<string>(key, { ns: "suffixes" });
+  const t = i18n.getFixedT(null, "suffixes");
 
   if (!unixTimestamp) {
-    return tSuffix("now");
+    return t("now");
   }
 
   const date = new Date(unixTimestamp * 1000);
@@ -20,9 +20,9 @@ export default function formatUnixDate(unixTimestamp: number): string {
   yesterdayStart.setDate(todayStart.getDate() - 1);
 
   if (date >= todayStart) {
-    return `${tSuffix("today")} ${date.toLocaleTimeString("ru-RU", options)}`;
+    return `${t("today")} ${date.toLocaleTimeString("ru-RU", options)}`;
   } else if (date >= yesterdayStart) {
-    return `${tSuffix("yesterday")} ${date.toLocaleTimeString("ru-RU", options)}`;
+    return `${t("yesterday")} ${date.toLocaleTimeString("ru-RU", options)}`;
   } else {
     const dateOptions: Intl.DateTimeFormatOptions = {
       day: "2-digit",
